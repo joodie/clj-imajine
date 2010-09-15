cdf23285c495709df4cae2c5c1f41b97dceeb24f
Joost Diepenmaat <joost@zeekat.nl>
2010-09-15 08:56:33 +0200

    added lein-clojars to dev-deps

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-imajine "0.1.0-SNAPSHOT"
+(defproject clj-imajine "0.1.0"
   :description "Convert and scale images"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 
                  [pdfbox/pdfbox "0.7.3"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
 
