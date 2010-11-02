9876101d14d4a1c5d6265bc8f2da896f91621769
Joost Diepenmaat <joost@zeekat.nl>
2010-11-02 14:04:34 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-imajine "0.1.3"
+(defproject clj-imajine "0.1.4"
   :description "Convert and scale images"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
